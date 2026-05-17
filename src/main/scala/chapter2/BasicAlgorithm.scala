1bd766a22ffc5aa1e0f61bd0cb59ded95d61627f
fe <flth741@gmail.com>
2026-05-17 11:36:06 +0200

    add fibonacci method

--- src/main/scala/chapter2/BasicAlgorithm.scala
+++ src/main/scala/chapter2/BasicAlgorithm.scala
@@ -13,8 +13,17 @@ object BasicAlgorithm {
     }
     go(n, 1)
 
+  // exercise 2.1
+  def fib(n: Int): Int =
+    @tailrec
+    def go(n: Int, curr: Int, next: Int): Int =
+      // println(s"go($n,$curr,$next)")
+      if n<=0 then curr
+      else go(n-1, next, curr + next)
+    go(n,0,1)
+
   def main(args: Array[String]): Unit = {
     println(factorial(5))
-
+    println(fib(6))
   }
 }

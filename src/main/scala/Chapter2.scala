edadd3a9e8d6aae27e0b9c97c217156107d9c552
fe <flth741@gmail.com>
2026-05-17 10:30:43 +0200

    implement factorial method

---
+++ src/main/scala/Chapter2.scala
@@ -0,0 +1,16 @@
+import scala.annotation.tailrec
+
+object Chapter2 {
+
+  def factorial(n: Int): Int =
+    @tailrec
+    def go(n: Int, acc: Int): Int =
+      if n <= 0 then acc
+      else go(n-1, n * acc)
+    go(n, 1)
+
+  def main(args: Array[String]): Unit = {
+    println(factorial(5))
+
+  }
+}

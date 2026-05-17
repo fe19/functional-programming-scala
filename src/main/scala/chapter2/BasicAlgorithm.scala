6fcc945c945255efd7734fe7463d3b9bff8eaadd
fe <flth741@gmail.com>
2026-05-17 12:37:24 +0200

    generic and polymorphic functions

--- src/main/scala/chapter2/BasicAlgorithm.scala
+++ src/main/scala/chapter2/BasicAlgorithm.scala
@@ -2,8 +2,19 @@ package chapter2
 
 import scala.annotation.tailrec
 
+/*
+ * learnings
+ * - recursive loops:         def loop(n: Int): Int
+ * - recursive methods:       def go(n: Int, c: Int, ...): Int
+ * - higher-order functions:  def format(n: Int, f: Int => Int)
+ * - generics:                def find[A]
+ * - signature:               often defines implementation
+ *
+ */
+
 object BasicAlgorithm {
 
+  // recursive method
   def factorial(n: Int): Int =
     @tailrec
     def go(n: Int, acc: Int): Int = {
@@ -22,8 +33,18 @@ object BasicAlgorithm {
       else go(n-1, next, curr + next)
     go(n,0,1)
 
+  // generic / polymorphic function / method
+  def findFirst[A](as: Array[A], p: A => Boolean): Int =
+    @tailrec
+    def loop(n: Int): Int =
+      if n >= as.length then -1
+      else if p(as(n)) then n
+      else loop(n+1)
+    loop(0)
+
   def main(args: Array[String]): Unit = {
     println(factorial(5))
     println(fib(6))
+    println(findFirst(Array(1,2,3), (x: Int) => x==2))
   }
 }

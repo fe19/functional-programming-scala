1cadd1556eab7b1258ef6f03f96522a4902b6e44
fe <flth741@gmail.com>
2026-06-23 20:28:08 +0200

    add variadic function

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -4,7 +4,7 @@ package chapter3
  * learnings
  * - concatenate              a ++ b
  * - covariance               List[+A]
- * -
+ * - variadic function        accept zero or more arguments. E.g., def apply[A](as: A*)
  * -
  * -
  * -
@@ -18,6 +18,10 @@ enum List[+A]:
   case Cons(head: A, tail: List[A])
 
 object List {
+
+  def apply[A](as: A*): List[A] =
+    if as.isEmpty then Nil
+    else Cons(as.head, apply(as.tail*))
 
   def main(args: Array[String]): Unit = {
     val l: List[String] = List.Cons("a", List.Cons("b", List.Nil))

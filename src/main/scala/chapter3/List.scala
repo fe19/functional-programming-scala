14aaca699888d84226f5ef7afbfb0af820a487b4
fe <flth741@gmail.com>
2026-06-23 20:17:15 +0200

    prepare learnings

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -1,5 +1,18 @@
 package chapter3
 
+/*
+ * learnings
+ * - concatenate              a ++ b
+ * -
+ * -
+ * -
+ * -
+ * -
+ * -
+ * -
+ * -
+ */
+
 enum List[+A]:
   case Nil
   case Cons(head: A, tail: List[A])

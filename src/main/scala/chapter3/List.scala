1aa3708da8001e0d5f560e54ff43cd680c4eac67
fe <flth741@gmail.com>
2026-06-27 16:24:51 +0200

    add sum function

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -5,7 +5,7 @@ package chapter3
  * - concatenate              a ++ b
  * - covariance               List[+A]
  * - variadic function        accept zero or more arguments. E.g., def apply[A](as: A*)
- * - pattern matching
+ * - pattern matching         ints match case Nil => 0  case Cons(x, xs) => x + sum(xs)
  * -
  * -
  * -
@@ -22,6 +22,10 @@ object List {
   def apply[A](as: A*): List[A] =
     if as.isEmpty then Nil
     else Cons(as.head, apply(as.tail*))
+
+  def sum(ints: List[Int]): Int = ints match
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
 
   def main(args: Array[String]): Unit = {
     val l: List[String] = List.Cons("a", List.Cons("b", List.Nil))

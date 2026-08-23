b3b47f5271d5a78b7b0c79536e69189dfbe24e71
fe <flth741@gmail.com>
2026-08-23 15:53:10 +0200

    add comments

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -146,11 +146,13 @@ object List {
   def filterFlatMap[A](as: List[A], f: A => Boolean): List[A] =
     flatMap(as, i => if f(i) then List(i) else Nil)
 
-  // ex 3.22
+  // ex 3.22 adding elements of lists
   def addLists(as: List[Int], bs: List[Int]): List[Int] = (as, bs) match
     case (Nil,_) => Nil
     case (_,Nil) => Nil
     case(Cons(h1,t1), Cons(h2,t2)) => Cons(h1 + h2, addLists(t1,t2))
+
+  // ex 3.23
 
   def main(args: Array[String]): Unit = {
     val l: List[String] = List.Cons("a", List.Cons("b", List.Nil))

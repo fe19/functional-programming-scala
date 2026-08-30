254a11869015035910a4983e80cfd254bbe3e852
fe <flth741@gmail.com>
2026-08-30 21:30:23 +0200

    add tree size function

--- src/main/scala/chapter3/Tree.scala
+++ src/main/scala/chapter3/Tree.scala
@@ -10,7 +10,11 @@ enum Tree[+A]:
   case Leaf(value: A)
   case Branch(left: Tree[A], right: Tree[A])
 
-  def size: Int = this match
+  def size: Int = this match // this is a method
     case Leaf(_) => 1
     case Branch(l, r) => 1 + l.size + r.size
 
+object Tree:
+  def size[A](t: Tree[A]): Int = t match // this is a function
+    case Leaf(_) => 1
+    case Branch(l, r) =>  1 + size(l) + size(r)
